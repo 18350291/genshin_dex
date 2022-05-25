55cf36a34ba514a777fb71527dfcf180200e7a99
Alexis Antonio Arano Castañeda <l18350291@gmail.com>
2022-05-25 10:00:23 -0500

    Primeros ajustes para obtener información de la Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-
 function App() {
+  const opciones = async () => {
+    const respuesta = await fetch("https://api.genshin.dev/");
+    const types = await respuesta.json();
+
+    console.log({ types });
+  };
+
+  opciones();
+
   return (
-    <h3>Hola mundo</h3>
+    <div>
+      <h3>Hola mundo</h3>
+      <select>
+        <option value="">Seleccione un elemento</option>
+      </select>
+    </div>
   );
 }
 
